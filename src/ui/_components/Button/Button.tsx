bdcd2260d740eb01cda5eb8fdb0fcc957d6385f7
Arrastia <arrastia@pm.me>
2022-07-26 21:54:29 +0200

    ADD - Button component

---
+++ src/ui/_components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Styles } from './Button.styles';
+
+type Props = {};
+
+export const Button = (props: Props) => {
+  return <Styles.Button>Save</Styles.Button>;
+};
