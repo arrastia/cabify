d0f401d6e7e264582397f0ee14d8826f7ecf6f5d
Arrastia <arrastia@pm.me>
2022-07-31 22:31:13 +0200

    ADD - discount & category params to Product entity

--- src/core/entities/Product/Product.ts
+++ src/core/entities/Product/Product.ts
@@ -1,17 +1,20 @@
+export type Category = 'MUG' | 'SHIRT' | 'CAP';
+export type Discount = '2_FOR_1_PROMO' | '5%_BULK_DISCOUNT' | null;
+
 export interface Product {
   blurHash: string;
+  category: Category;
   code: string;
   description: string;
-  id: number;
+  discount: Discount;
+  id: string;
   name: string;
   picture: string;
   price: number;
-  thumbnail: string;
-  thumbnailBlurHash: string;
 }
 
 export class Product {
-  constructor({ blurHash, code, description, id, name, picture, price, thumbnail, thumbnailBlurHash }: Product) {
+  constructor({ blurHash, code, description, id, name, picture, price }: Product) {
     this.blurHash = blurHash;
     this.code = code;
     this.description = description;
@@ -19,7 +22,5 @@ export class Product {
     this.name = name;
     this.picture = picture;
     this.price = price;
-    this.thumbnail = thumbnail;
-    this.thumbnailBlurHash = thumbnailBlurHash;
   }
 }
