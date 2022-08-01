d296af228b459ccc658cca293667d8e55a281551
Arrastia <arrastia@pm.me>
2022-08-01 22:37:16 +0200

    UPDATE - Checkout store

--- src/ui/stores/CheckoutStore.ts
+++ src/ui/stores/CheckoutStore.ts
@@ -1,31 +1,11 @@
 import { atom, atomFamily, selector } from 'recoil';
 
 import { ProductService } from 'core/services/Product';
-import type { Product } from 'core/entities/Product';
-
-// import type { Product } from 'core/entities/Product';
+import type { Category } from 'core/entities/Product/Product';
 
 export const allProductsState = atom<number>({
   key: 'allProductsState',
   default: 0
-});
-
-export const allState = atom<Product[]>({
-  key: 'allState',
-  default: []
-});
-
-export const selectorState = selector({
-  key: 'selectorState',
-  get:
-    ({ get }) =>
-    (key: string) => {
-      const all = get(allState);
-
-      const test = groupByKey(all, 'code')[key];
-
-      return test;
-    }
 });
 
 export const productQuantityState = atomFamily({
@@ -33,15 +13,18 @@ export const productQuantityState = atomFamily({
   default: () => 0
 });
 
+export const previousQuantityState = selector({
+  key: 'previousQuantityState',
+  get:
+    ({ get }) =>
+    (category: Category) => {
+      const productId = get(productQuantityState(category));
+
+      return productId;
+    }
+});
+
 export const productState = selector({
   key: 'productState',
   get: async () => await ProductService.allProducts()
 });
-
-function groupByKey<T>(array: T[], key: keyof T) {
-  return array.reduce((hash: any, obj: any) => {
-    if (obj[key] === undefined) return hash;
-
-    return Object.assign(hash, { [obj[key]]: (hash[obj[key]] || []).concat(obj) });
-  }, {});
-}
