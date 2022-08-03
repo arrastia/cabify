4ecff3fb42ac722a2b02d307c71a258a2fa36ccc
Arrastia <arrastia@pm.me>
2022-08-03 22:52:21 +0200

    ADD - Applied discounts component

---
+++ src/ui/components/AppliedDiscounts/AppliedDiscounts.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {};
+
+export const AppliedDiscounts = (props: Props) => {
+  return (
+    <div>
+      <h2>Discounts</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales semper elit sit amet interdum. Praesent volutpat sed elit vel consectetur.
+        Nulla tempus tincidunt ex, sit amet semper ipsum imperdiet varius. In rutrum aliquam nisl, sagittis faucibus felis bibendum id.
+      </p>
+    </div>
+  );
+};
---
+++ src/ui/components/AppliedDiscounts/index.ts
@@ -0,0 +1,1 @@
+export { AppliedDiscounts } from './AppliedDiscounts';
